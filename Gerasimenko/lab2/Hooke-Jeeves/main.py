8bf1bc91ae5b9a04e86824b6a7fbffa8a2dc5144
NIXIN <Zadvornov.Aleksandr@gmail.com>
2010-11-24 14:10:35 +0300

    add input step

--- Gerasimenko/lab2/Hooke-Jeeves/main.py
+++ Gerasimenko/lab2/Hooke-Jeeves/main.py
@@ -11,15 +11,19 @@ def input_data(text):
         
     return data
 
+
 text = "%s" % unicode('Введите координату X начальной точки: ', 'utf-8')
 x0_X = input_data(text)
 text = "%s" % unicode('Введите координату Y начальной точки: ', 'utf-8')
 x0_Y = input_data(text)
 
+text = "%s" % unicode('Введите шаг по координате X: ', 'utf-8')
+step_X = input_data(text)
+text = "%s" % unicode('Введите шаг по координате Y: ', 'utf-8')
+step_Y = input_data(text)
+
+
 x = {}
 x = {0:[x0_X,x0_Y]}
 
 print "x = {0}    y = {1}".format(x0_X, x0_Y)
-print x
-print x[0]
-print x[0][0]
